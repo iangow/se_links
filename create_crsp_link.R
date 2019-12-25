29a292484cc38e245befd809e50b5cf695d6ec62
Ian Gow <iandgow@gmail.com>
2019-12-25 11:09:24 +1100

    Tweaks to crsp_link code.

--- create_crsp_link.R
+++ create_crsp_link.R
@@ -1,12 +1,11 @@
 # Create_crsp_link.R
 library(dplyr, warn.conflicts = FALSE)
-library(RPostgreSQL)
-library(googlesheets)
+library(DBI)
 library(readr)
 
-pg <- dbConnect(PostgreSQL())
+pg <- dbConnect(RPostgreSQL::PostgreSQL())
 
-dbGetQuery(pg, read_file('crsp_link.sql'))
+rs <- dbGetQuery(pg, read_file('crsp_link.sql'))
 
 db_comment <- paste0("CREATED USING create_crsp_link.R ON ", Sys.time())
-dbGetQuery(pg, sprintf("COMMENT ON TABLE streetevents.crsp_link IS '%s';", db_comment))
+rs <- dbExecute(pg, sprintf("COMMENT ON TABLE streetevents.crsp_link IS '%s';", db_comment))
