a1c8860ff57d97b27be6039735810c5331435e65
Ian Gow <iandgow@gmail.com>
2020-01-11 00:19:26 +1100

    Code to create "new" csrp_link.

---
+++ create_crsp_link_new.R
@@ -0,0 +1,39 @@
+library(dplyr, warn.conflicts = FALSE)
+library(DBI)
+
+pg <- dbConnect(RPostgres::Postgres())
+
+rs <- dbExecute(pg, "SET search_path TO streetevents")
+
+company_ids <- tbl(pg, "company_ids")
+calls <- tbl(pg, "calls")
+manual_permno_matches <- tbl(pg, "manual_permno_matches")
+
+stocknames <- tbl(pg, sql("SELECT * FROM crsp.stocknames"))
+
+crsp_link_auto <-
+    company_ids %>%
+    mutate(ncusip = substr(cusip, 1, 8)) %>%
+    select(file_name, last_update, ncusip) %>%
+    inner_join(stocknames, by = "ncusip") %>%
+    select(file_name, last_update, permno) %>%
+    distinct() %>%
+    compute()
+
+rs <- dbExecute(pg, "DROP TABLE IF EXISTS crsp_link_new")
+
+crsp_link <-
+    crsp_link_auto %>%
+    left_join(manual_permno_matches, by = "file_name") %>%
+    mutate(permno = coalesce(permno.y, permno.x)) %>%
+    select(file_name, last_update, permno) %>%
+    compute(name = "crsp_link_new", temporary = FALSE)
+
+rs <- dbExecute(pg, "ALTER TABLE crsp_link_new OWNER TO streetevents")
+rs <- dbExecute(pg, "GRANT SELECT ON crsp_link_new TO streetevents_access")
+
+rs <- dbExecute(pg, "CREATE INDEX ON crsp_link (file_name)")
+
+db_comment <- paste0("CREATED USING iangow/se_links/create_crsp_link_new.R ON ",
+                     Sys.time())
+rs <- dbExecute(pg, sprintf("COMMENT ON TABLE crsp_link_new IS '%s';", db_comment))
